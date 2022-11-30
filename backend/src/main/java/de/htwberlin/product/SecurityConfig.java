8caca18c000cf0cc4b88169c653e09816a1b9818
MarvinLink <82515239+LinkionTheHead@users.noreply.github.com>
2022-11-30 19:16:22 +0100

    Fetch Problem2

--- backend/src/main/java/de/htwberlin/product/SecurityConfig.java
+++ backend/src/main/java/de/htwberlin/product/SecurityConfig.java
@@ -1,19 +1,8 @@
 package de.htwberlin.product;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
-
-
-@EnableWebSecurity
-public class SecurityConfig {
+//
+//@EnableWebSecurity
+//public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
