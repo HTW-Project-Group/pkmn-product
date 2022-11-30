929525bc1a090a95eb974e3cd96b9c11f8292213
MarvinLink <82515239+LinkionTheHead@users.noreply.github.com>
2022-11-30 19:32:24 +0100

    Fetch Problem3

--- backend/src/main/java/de/htwberlin/product/BackendApplication.java
+++ backend/src/main/java/de/htwberlin/product/BackendApplication.java
@@ -16,7 +16,7 @@ public class BackendApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
             }
         };
     }
--- backend/src/main/java/de/htwberlin/product/controller/ProductController.java
+++ backend/src/main/java/de/htwberlin/product/controller/ProductController.java
@@ -3,12 +3,13 @@ package de.htwberlin.product.controller;
 import de.htwberlin.product.entity.ProductEntity;
 import de.htwberlin.product.exception.ProductNotFoundException;
 import de.htwberlin.product.service.ProductService;
-import java.util.List;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
@@ -18,11 +19,13 @@ public class ProductController {
   private ProductService productService;
 
   @GetMapping
+  @CrossOrigin
   public ResponseEntity<List<ProductEntity>> getProducts() {
     return new ResponseEntity<>(productService.findAllProducts(), HttpStatus.OK);
   }
 
   @GetMapping("/{id}")
+  @CrossOrigin
   public ResponseEntity<ProductEntity> getProductById(@PathVariable("id") UUID id)
       throws ProductNotFoundException {
     return new ResponseEntity<>(productService.findProductById(id), HttpStatus.OK);
