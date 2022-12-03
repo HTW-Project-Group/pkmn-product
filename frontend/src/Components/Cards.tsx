fcd53bf6a63c9823b9b8ac0e4cbcd0f8523832e4
MarvinLink <82515239+LinkionTheHead@users.noreply.github.com>
2022-12-03 20:10:13 +0100

    Startseite 3.0

--- frontend/src/Components/Cards.tsx
+++ frontend/src/Components/Cards.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { CardActionArea, CardContent, CardMedia } from "@mui/material";
+import {CardActionArea, CardContent, CardMedia, createTheme, ThemeProvider} from "@mui/material";
 import Typography from "@mui/material/Typography";
+
 
 type cardprops = {
     name: string;
@@ -9,32 +10,66 @@ type cardprops = {
     price: number;
 };
 
-export default function Cards({ name, image, id, price }: cardprops) {
-  return (
-    <CardActionArea
-      sx={{
-        borderRadius: 0,
-        transition: "0.2s",
-        "&:hover": {
-          transform: "scale(1.03)",
-        },
-        padding: 2,
-        zIndex: "0",
-        boxShadow: "0 8px 16px 0 #BDC9D7",
-      }}
-    >
-      <CardMedia component="img" height="auto" width="auto" image={image}/>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {name}
-        </Typography>
-        <Typography gutterBottom variant="h6" component="div">
-          {price}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Hier steht die Beschreibung
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-  );
+const theme1 = createTheme();
+
+theme1.typography.body2 = {
+    fontSize: '0.5rem',
+    '@media (min-height: 70px )': {
+        fontSize: '10px',
+    }
+};
+const theme2 = createTheme();
+
+theme2.typography.h5 = {
+    fontSize: '1.5rem',
+    '@media (min-height: 70px )': {
+        fontSize: '15px',
+    }
+};
+const theme3 = createTheme();
+
+theme3.typography.h6 = {
+    fontSize: '2rem',
+    '@media (min-height: 70px )': {
+        fontSize: '13px',
+    }
+};
+export default function Cards({name, image, id, price}: cardprops) {
+    return (
+        <div /* style={{overflow:"hidden"}}*/>
+
+            <CardActionArea
+                sx={{
+                    borderRadius: 0,
+                    transition: "0.2s",
+                    "&:hover": {
+                        transform: "scale(1.03)",
+                    },
+                    padding: 1,
+                    zIndex: "0",
+                    boxShadow: "0 8px 16px 0 #BDC9D7",
+                }}
+            >
+                <CardMedia component="img" height="auto" width="auto" image={image}/>
+                <CardContent sx={{padding: 0, paddingTop: 1}}>
+                    <ThemeProvider theme={theme2}>
+                        <Typography height="auto" width="auto" gutterBottom variant="h5" component="div">
+                            {name}
+                        </Typography>
+                    </ThemeProvider>
+                    <ThemeProvider theme={theme3}>
+
+                        <Typography height="auto" width="auto" gutterBottom variant="h6" component="div">
+                            {price}
+                        </Typography>
+                    </ThemeProvider>
+                    <ThemeProvider theme={theme1}>
+                        <Typography height="auto" width="auto" variant="body2" color="text.secondary">
+                            Hier steht die Beschreibung
+                        </Typography>
+                    </ThemeProvider>
+                </CardContent>
+            </CardActionArea>
+        </div>
+    );
 }
