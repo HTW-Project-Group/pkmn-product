898f1e6bef62753ae68e80b951d2f7e243901396
Alexander Mai <83711446+GodPepe7@users.noreply.github.com>
2022-12-09 21:11:05 +0100

    PKMN-33 cypress tests

---
+++ frontend/cypress/component/Header.cy.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import Header from '../../src/Components/Header/Header';
+
+describe('RecommendedProducts.cy.ts', () => {
+    it('playground', () => {
+        cy.mount(<Header/>)
+    })
+})
