0541742da9502340f462fa758c34f88aa99d5ad9
corvin-rose <business@corvin-rose.de>
2022-12-11 01:17:17 +0100

    PKMN-33: Fixed Cypress double execution

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import * as React from "react";
-import App from "./App";
-
-test("renders cam header", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/PKMN/i);
-  expect(linkElement).toBeInTheDocument();
-});
