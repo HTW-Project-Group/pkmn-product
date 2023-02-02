6e562ec2b52a2fd6c62de13ba2ea90040ef6cb6e
Alexander Mai <83711446+GodPepe7@users.noreply.github.com>
2023-02-02 17:06:01 +0100

    PKMN-34 added mocks for cypress tests

--- frontend/cypress/Components/ProductDetail.cy.tsx
+++ frontend/cypress/Components/ProductDetail.cy.tsx
@@ -1,12 +1,77 @@
 import * as React from 'react'
 import ProductDetail from "../../src/Components/ProductDetail"
+import Card from "../../src/Model/Card"
 
 describe('Header.cy.ts', () => {
-    it('renders random pkmn details', () => {
-        cy.request("http://localhost:8080/v1/products?amount=1").then((data) => {
-            const product = data.body[0];
-            cy.mount(<ProductDetail description={product.description} condition={product.condition} id={product.id}
-                                    name={product.name} pokemonId={product.pokemonId} price={product.price}/>)
+    it('renders a pokemons details', () => {
+        const productMock: Card = {
+            condition: 10,
+            pokemonId: "swsh9-166",
+            id: "64f75d2e-3677-4ed8-84c9-70e51ded0aec",
+            name: "Arceus V",
+            price: 271.72,
+            description: "Sunt sed voluptas sit et modi minus nihil qui id tempore quo voluptatum sequi enim est aut in dolorem dolores deserunt porro enim reiciendis quia soluta molestiae sed rerum ratione quia perspiciatis autem ut exercitationem vel assumenda ipsum labore praesentium architecto et atque est eum maxime illum illo doloribus rerum molestiae consequatur autem asperiores sequi eos nostrum et cupiditate nemo nemo eveniet ea culpa eos iusto placeat sapiente occaecati sequi repellendus sint velit minima veritatis est eligendi provident suscipit voluptatem in sint dolor accusamus deleniti at itaque suscipit sapiente omnis at qui ut temporibus fugiat voluptas id nulla nulla natus."
+        }
+        cy.mount(<ProductDetail description={productMock.description} condition={productMock.condition} id={productMock.id}
+                                name={productMock.name} pokemonId={productMock.pokemonId} price={productMock.price}/>)
+        cy.intercept('GET','/v1/pokemon/**', {
+            statusCode: 201,
+            body: {
+                id: "swsh9-166",
+                name: "Arceus V",
+                supertype: "Pokémon",
+                subtypes: [
+                    "Basic",
+                    "V"
+                ],
+                level: null,
+                hp: "220",
+                types: [
+                    "Colorless"
+                ],
+                attacks: [
+                    {
+                        name: "Trinity Charge",
+                        cost: [
+                            "Colorless",
+                            "Colorless"
+                        ],
+                        convertedEnergyCost: 2,
+                        damage: "",
+                        text: "Search your deck for up to 3 basic Energy cards and attach them to your Pokémon V in any way you like. Then, shuffle your deck."
+                    },
+                    {
+                        name: "Power Edge",
+                        cost: [
+                            "Colorless",
+                            "Colorless",
+                            "Colorless"
+                        ],
+                        convertedEnergyCost: 3,
+                        damage: "130",
+                        text: ""
+                    }
+                ],
+                weaknesses: [
+                    {
+                        type: "Fighting",
+                        value: "×2"
+                    }
+                ],
+                resistances: null,
+                set: {
+                    id: "swsh9",
+                    name: "Brilliant Stars",
+                    series: "Sword & Shield",
+                    printedTotal: 172,
+                    total: 186,
+                    ptcgoCode: "BRS",
+                    releaseDate: "2022/02/25"
+                },
+                number: "166",
+                artist: "kawayoo",
+                rarity: "Rare Ultra"
+            }
         })
     })
 })
