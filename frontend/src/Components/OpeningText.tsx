e3dad33f3f60930b81cc3977abb45345af3d542c
corvin-rose <business@corvin-rose.de>
2023-02-13 01:41:55 +0100

    PKMN-0: Refactored Start page

---
+++ frontend/src/Components/OpeningText.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+export default function OpeningText() {
+  return (
+    <div className="opening-text">
+      <div className="banner blue">
+        <p>
+          Welcome to the world of Pokémon Trading Cards! Here at PKMN, we are
+          dedicated to bringing you the very best in Pokémon collectibles and
+          accessories. Whether you&apos;re a seasoned player or just starting
+          out, we have everything you need to build the ultimate deck and become
+          a Pokémon master.
+        </p>
+      </div>
+
+      <div className="banner red reverse">
+        <p>
+          Our collection features a wide variety of cards, from the latest sets
+          to rare and hard-to-find treasures. With our competitive prices, fast
+          shipping, and exceptional customer service, there&apos;s never been a
+          better time to get into the world of Pokémon Trading Cards.
+        </p>
+      </div>
+      <div className="banner blue">
+        <p>
+          So why wait? Start your journey today and discover the thrill of
+          collecting and battling with the most iconic creatures in the Pokémon
+          universe!
+        </p>
+      </div>
+    </div>
+  );
+}
--- frontend/src/Pages/App.tsx
+++ frontend/src/Pages/App.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import Header from "../Components/Header/Header";
 import ContentBanner from "../Components/ContentBanner";
 import RecommendedProducts from "../Components/Product/RecommendedProducts";
+import OpeningText from "../Components/OpeningText";
 import "../Css/App.css";
 
 export default function App() {
@@ -9,26 +10,7 @@ export default function App() {
     <div>
       <Header />
       <ContentBanner />
-      <div className="opening-text">
-        <p>
-          Welcome to the world of Pokémon Trading Cards! Here at PKMN, we are
-          dedicated to bringing you the very best in Pokémon collectibles and
-          accessories. Whether you&apos;re a seasoned player or just starting
-          out, we have everything you need to build the ultimate deck and become
-          a Pokémon master.
-        </p>
-        <p>
-          Our collection features a wide variety of cards, from the latest sets
-          to rare and hard-to-find treasures. With our competitive prices, fast
-          shipping, and exceptional customer service, there&apos;s never been a
-          better time to get into the world of Pokémon Trading Cards.
-        </p>
-        <p>
-          So why wait? Start your journey today and discover the thrill of
-          collecting and battling with the most iconic creatures in the Pokémon
-          universe!
-        </p>
-      </div>
+      <OpeningText />
       <RecommendedProducts />
     </div>
   );
