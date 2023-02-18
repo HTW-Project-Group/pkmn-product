ead080ef1cb870f76d41112c5b0110f77236b948
corvin-rose <business@corvin-rose.de>
2023-02-18 20:49:30 +0100

    PKMN-39: Prepared for RabbitMQ

---
+++ backend/src/main/java/de/htwberlin/port/product/QueueController.java
@@ -0,0 +1,19 @@
+package de.htwberlin.port.product;
+
+import de.htwberlin.core.appservice.dto.ProductDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/v1/queue")
+public class QueueController {
+
+    @PostMapping("/basket/add")
+    public ResponseEntity<ProductDto> addProductToBasketQueue() {
+        return null;
+    }
+}
