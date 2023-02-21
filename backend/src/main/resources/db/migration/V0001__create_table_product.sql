1abed61d6e14f74d620493b8ea152263c646d55f
corvin-rose <business@corvin-rose.de>
2023-02-21 09:41:05 +0100

    PKMN-0: Adjust possible length of pokemon name to prevent errors

--- backend/src/main/resources/db/migration/V0001__create_table_product.sql
+++ backend/src/main/resources/db/migration/V0001__create_table_product.sql
@@ -1,7 +1,7 @@
 create table product
 (
     id          UUID               NOT NULL,
-    name        VARCHAR(50)        NOT NULL,
+    name        VARCHAR(100)       NOT NULL,
     price       NUMERIC(10, 2) DEFAULT 0,
     description TEXT,
     condition   INTEGER CHECK (condition between 1 and 10),
